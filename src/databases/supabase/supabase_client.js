6c20e52ef810f7e8383f29757c2d1e6105738d6b
yosho <yleecruz@gmail.com>
2024-11-06 13:46:40 -0500

    vercel.json app.tsq

--- src/databases/supabase/supabase_client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const supabase_js_1 = require("@supabase/supabase-js");
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
-exports.default = supabase;
--- src/routes/issueRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const issueController_1 = require("../controllers/issueController");
-const router = (0, express_1.Router)();
-router.get("/", issueController_1.getAll);
-router.post("/", issueController_1.create);
-router.put("/:id", issueController_1.resolve);
-exports.default = router;
