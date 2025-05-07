4d7900a3a788a76bd270a2129a5fb4e3637b8cd9
yosho <yleecruz@gmail.com>
2025-05-07 14:06:42 -0400

    stats imprvoe

--- src/api/v1/routes/stats-router.ts
+++ src/api/v1/routes/stats-router.ts
@@ -1,26 +1,28 @@
 import { Router } from "express";
 import { mysql_db } from "../models/myslq/mysql_db";
 import { prisma_db } from "../models/prisma/prisma_db";
-
+import { authMiddleware } from "../middlewares/auth-middleware";
 const statsRouter = Router();
 
-statsRouter.get("/", async (req, res) => {
+statsRouter.get("/", authMiddleware, async (req, res) => {
 	const sales = await mysql_db.stats.getSalesStats();
 	res.json(sales);
 });
-statsRouter.get("/daily-sales", async (req, res) => {
-	const sales = await mysql_db.stats.getDailySalesByAgency(2);
+statsRouter.get("/daily-sales", authMiddleware, async (req, res) => {
+	const user = req.user;
+	const sales = await mysql_db.stats.getDailySalesByAgency(user.agencyId);
 	res.json(sales);
 });
-statsRouter.get("/employees-sales", async (req, res) => {
-	const sales = await mysql_db.stats.getEmployeeSales(2);
+statsRouter.get("/employees-sales", authMiddleware, async (req, res) => {
+	const user = req.user;
+	const sales = await mysql_db.stats.getEmployeeSales(user.agencyId);
 	res.json(sales);
 });
-statsRouter.get("/containers-status", async (req, res) => {
+statsRouter.get("/containers-status", authMiddleware, async (req, res) => {
 	const containersStatus = await prisma_db.stats.containersStatus();
 	res.json(containersStatus);
 });
-statsRouter.get("/delivery-todays", async (req, res) => {
+statsRouter.get("/delivery-todays", authMiddleware, async (req, res) => {
 	const deliveryTodays = await prisma_db.stats.deliveryTodays();
 	res.json(deliveryTodays);
 });
